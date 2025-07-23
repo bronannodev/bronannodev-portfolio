04a1e6dae75650826c3e114b09563e6385934090
Santiago Bonanno <bonannosantiago@gmail.com>
2025-07-23 20:53:26 -0300

    Actualizo sección de projects

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -33,7 +33,7 @@ export const Projects = () => {
                 </div>
                 <div className="flex justify-between items-center">
                   <a
-                    href="#home"
+                    href="https://github.com/bronannodev/bronannodev-portfolio"
                     className="text-blue-400 hover:text-blue-300 transition-colors my-4"
                   >
                     Link repositorio →
